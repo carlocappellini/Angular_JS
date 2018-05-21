744adcaafe5f3764fafbe21c44a5b2d272a55049
Bolimbo <carlo.cappellini@yahoo.com>
2018-05-20 22:26:33 -0500

    MVW

---
+++ app.js
@@ -0,0 +1,16 @@
+// MODULE
+var angularApp = angular.module('angularApp', []);
+
+// CONTROLLERS will be the model , code inside here , we can pass it as an custom ng attribute  = controller  to any element , angular will look for controller inside myApp with that custom ng name 'mainController'
+
+/// any code inside the element that contains the ng-controller will be associated eith controlling the html inside of that element
+
+
+//controllers will be the models
+angularApp.controller('mainController', ['$scope', function ($scope) {
+    
+}]);
+
+
+
+
